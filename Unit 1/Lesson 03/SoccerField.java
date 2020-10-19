a89de69a7bb143015701ae2cd504a4010cd06356
kevinchen220 <68040593+kevinchen220@users.noreply.github.com>
2020-10-19 14:15:04 -0400

    Create SoccerField.java

---
+++ Unit 1/Lesson 03/SoccerField.java
@@ -0,0 +1,17 @@
+/*
+SoccerField
+Created by: Kevin Chen
+Last Modified: September 29, 2020
+Description: Extension of City to create a soccer field.
+ */
+
+import becker.robots.*;
+
+public class SoccerField extends City
+{
+    //Default Constructor
+    public SoccerField()
+    {
+        super("SoccerFieldTemplate.txt"); //Imported map
+    }
+}
